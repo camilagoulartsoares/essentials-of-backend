374072b348109427801652456b5e6a3eac82a955
Camila Soares <camilagoulartsoares@yahoo.com>
2026-01-08 07:26:30 -0300

    inicia código Java com estrutura básica

---
+++ java-essentials/Start.java
@@ -0,0 +1,11 @@
+public class Start {
+    public static void main(String[] args) {
+
+        String pais = "Brasil";
+        int idade = 23;
+
+        System.out.println(pais);
+        System.out.println(idade);
+
+    }
+}
